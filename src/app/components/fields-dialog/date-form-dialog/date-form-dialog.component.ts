9158b6eb1666c59e1e973085a6d041787cbaa752
Samar <samar.daghari@esprit.tn>
2024-04-05 11:12:45 +0100

    field-options

--- src/app/components/fields-dialog/date-form-dialog/date-form-dialog.component.ts
+++ src/app/components/fields-dialog/date-form-dialog/date-form-dialog.component.ts
@@ -21,8 +21,16 @@ export class DateFormDialogComponent implements OnInit {
     this.form = this.fb.group({
       label: [this.data.label, Validators.required],
       placeholder: [this.data.placeholder],
-      minLength: [this.data.minLength, Validators.min(0)],
-      maxLength: [this.data.maxLength, Validators.min(0)],
+      label_position: [this.data.label_position],
+      custom_css: [this.data.custom_css],
+      hidden: [this.data.hidden],
+      hide_label: [this.data.hide_label],
+      disabled: [this.data.disabled],
+      required: [this.data.required],
+      error_label: [this.data.error_label],
+      custom_error_message: [this.data.custom_error_message],
+      property_name: [this.data.property_name],
+      field_tags: [this.data.field_tags]
     });
   }
 
--- src/app/components/fields-dialog/form-dialog-checkbox/form-dialog-checkbox.component.ts
+++ src/app/components/fields-dialog/form-dialog-checkbox/form-dialog-checkbox.component.ts
@@ -27,6 +27,15 @@ export class FormDialogCheckboxComponent implements OnInit {
   ngOnInit() {
     this.form = this.fb.group({
       label: [this.data.label, Validators.required],
+      custom_css: [this.data.custom_css],
+      hidden: [this.data.hidden],
+      hide_label: [this.data.hide_label],
+      disabled: [this.data.disabled],
+      required: [this.data.required],
+      error_label: [this.data.error_label],
+      custom_error_message: [this.data.custom_error_message],
+      property_name: [this.data.property_name],
+      field_tags: [this.data.field_tags]
     });
     this.form.valueChanges.subscribe(() => {
       this.updateFields();
