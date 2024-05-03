6d9573fd2f9d93aa45b600645dd0cac1d5ee1396
Samar <samar.daghari@esprit.tn>
2024-05-03 14:42:45 +0100

    all fields translation

--- src/app/components/fields-dialog/date-form-dialog/date-form-dialog.component.ts
+++ src/app/components/fields-dialog/date-form-dialog/date-form-dialog.component.ts
@@ -2,6 +2,7 @@ import {Component, Inject, OnInit, ViewChild} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
 import {FormlyFieldConfig, FormlyFormOptions} from "@ngx-formly/core";
+import {TranslationService} from "../../../services/translation.service";
 
 @Component({
   selector: 'app-date-form-dialog',
@@ -18,34 +19,44 @@ export class DateFormDialogComponent implements OnInit {
   options: FormlyFormOptions = {};
   model: any = {};
   selectedTabIndex = 0; // Default tab index
+  translations: any = {};
 
   constructor(
     private fb: FormBuilder,
     public dialogRef: MatDialogRef<DateFormDialogComponent>,
+    private translationService: TranslationService,
     @Inject(MAT_DIALOG_DATA) public data: any
   ) {}
   // tslint:disable-next-line:typedef
   ngOnInit() {
     this.form = this.fb.group({
-      label: [this.data.label, Validators.required],
+      label_fr: [this.data.label_fr, Validators.required],
+      label_ar: [this.data.label_ar, Validators.required],
       placeholder: [this.data.placeholder],
       label_position: [this.data.label_position],
       custom_css: [this.data.custom_css],
       hidden: [this.data.hidden],
-      hide_label: [this.data.hide_label],
+      hide_label_fr: [this.data.hide_label_fr],
+      hide_label_ar: [this.data.hide_label_ar],
       disabled: [this.data.disabled],
       required: [this.data.required],
       error_label: [this.data.error_label],
       custom_error_message: [this.data.custom_error_message],
-      property_name: [this.data.property_name],
+      property_name: [this.generatePropertyName(this.data.label_fr)],
       field_tags: [this.data.field_tags]
     });
 
     // Subscribe to label changes to update property name
-    this.form.get('label').valueChanges.subscribe((label: string) => {
+    this.form.get('label_fr').valueChanges.subscribe((label: string) => {
       const propertyNameControl = this.form.get('property_name');
       propertyNameControl.setValue(this.generatePropertyName(label));
     });
+
+    this.translationService.getCurrentLanguage().subscribe((language: string) => {
+      this.loadTranslations();
+    });
+
+    this.loadTranslations();
 
     this.form.valueChanges.subscribe(() => {
       this.updateFields();
@@ -87,20 +98,38 @@ export class DateFormDialogComponent implements OnInit {
 
     return propertyName;
   }
+
+  loadTranslations() {
+    this.translationService.getCurrentLanguage().subscribe((language: string) => {
+      this.translationService.loadTranslations(language).subscribe((translations: any) => {
+        console.log('Loaded translations:', translations);
+        this.translations = translations;
+      });
+    });
+  }
+
   updateTags(inputValue: string): void {
     const tagsArray = inputValue.split(',').map(tag => tag.trim());
     this.form.get('field_tags').setValue(tagsArray);
   }
   updateFields(): void {
-    const labelHidden = this.form.get('hide_label').value;
+    const labelFrHidden = this.form.get('hide_label_fr').value;
+    const labelArHidden = this.form.get('hide_label_ar').value;
     const inputHidden = this.form.get('hidden').value;
     const inputDisabled = this.form.get('disabled').value;
+
+    this.translationService.getCurrentLanguage().subscribe((currentLanguage: string) => {
+      const label_fr = this.form.get('label_fr').value;
+      const label_ar = this.form.get('label_ar').value;
+      const textLabel = currentLanguage === 'ar' ? label_ar : label_fr;
 
     this.newField = {
       type: 'input',
       key: 'key1',
       templateOptions: {
-        label: labelHidden ? null : this.form.get('label').value,
+        label:textLabel,
+        label_fr: labelFrHidden ? null : this.form.get('label_fr').value,
+        label_ar: labelArHidden ? null : this.form.get('label_ar').value,
         type: 'text',
         placeholder: this.form.get('placeholder').value,
         disabled: inputDisabled,
@@ -110,10 +139,13 @@ export class DateFormDialogComponent implements OnInit {
       },
       hide: inputHidden,
       expressionProperties: {
-        'templateOptions.hideLabel': () => labelHidden
+        'templateOptions.hideLabel_fr': () => labelFrHidden,
+        'templateOptions.hideLabel_ar': () => labelArHidden
       },
 
     };
+    });
+
   }
 
 
