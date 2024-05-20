6bff89b8d292547eaf82e038008ac9a9e269c2f4
Samar <samar.daghari@esprit.tn>
2024-05-20 15:14:58 +0100

    adding the alert message to form submit

--- src/app/models/FormTemplate.ts
+++ src/app/models/FormTemplate.ts
@@ -4,6 +4,7 @@ import {FormlyFieldConfig} from '@ngx-formly/core';
 export class FormTemplate {
   title ?: string;
   description?: string;
+  alert_message ?: string;
   version?: number;
   fieldIds?: string[];
   createdAt?: Date;
