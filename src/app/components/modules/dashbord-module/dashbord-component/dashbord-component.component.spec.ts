27bb9db64c2e0b6c9ea873621445107f2467edd2
soulaima537 <61362270+soulaima537@users.noreply.github.com>
2024-06-05 08:48:33 +0100

    chartjs

--- src/app/components/modules/dashbord-module/dashbord-component/dashbord-component.component.spec.ts
+++ src/app/components/modules/dashbord-module/dashbord-component/dashbord-component.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { DashboardComponent } from './dashboard.component';
+import { DashbordComponentComponent } from './dashbord-component.component';
 
-describe('DashboardComponent', () => {
-  let component: DashboardComponent;
-  let fixture: ComponentFixture<DashboardComponent>;
+describe('DashbordComponentComponent', () => {
+  let component: DashbordComponentComponent;
+  let fixture: ComponentFixture<DashbordComponentComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ DashboardComponent ]
+      declarations: [ DashbordComponentComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(DashboardComponent);
+    fixture = TestBed.createComponent(DashbordComponentComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- src/app/components/modules/dashbord-module/dashbord-module.module.ts
+++ src/app/components/modules/dashbord-module/dashbord-module.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {DashbordRoutingModule} from './dashbord-routing.module';
+import { DashbordComponentComponent } from './dashbord-component/dashbord-component.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ DashbordComponentComponent],
   imports: [
     CommonModule,
     DashbordRoutingModule
